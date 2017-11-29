ecbc1bc4b273261b19ee766323b4a521c1301b52
Jeff Dyer <jeffdyer@acm.org>
2017-11-29 11:52:35 -0800

    debug

--- src/compile.js
+++ src/compile.js
@@ -670,6 +670,7 @@ export let compiler = (function () {
             });
           };
         }, (ids) => {
+          console.log("compile() ids.length=" + ids.length);
           // Got codeIDs for children of current node. Now create a map of
           // saveIDs and codeIDs.
           let idMap = [];
@@ -692,6 +693,7 @@ export let compiler = (function () {
             }
           });
           putData(idMap, (id) => {
+            console.log("putData() id=" + id);
             // Compile idMap using L113 and store its ID.
             node.link = id;
             node.index = index;
